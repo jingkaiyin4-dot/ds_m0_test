25aef286812c1d455abc12442eb181350420c7d8
Jackson <jingkaiyin4@gmail.com>
2026-04-14 19:32:22 +0800

    ignore generated build artifacts

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -Wl,-ldevice.cmd.genlibs -Wl,-llibc.a -Wl,-l"C:/TI/mspm0_sdk_2_05_01_00/source/ti/driverlib/lib/ticlang/m0p/mspm0g1x0x_g3x0x/driverlib.a"
-
